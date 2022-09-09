f8d03a82d11c24af14ce9bdbd5530d6b4ad1ca9d
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-09 04:47:40 -0700

    1-8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ *
+ *Description: prints a given quote
+ *
+ *Return: 1 if successful
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
