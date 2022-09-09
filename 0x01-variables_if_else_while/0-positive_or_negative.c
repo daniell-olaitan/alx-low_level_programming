ceebc6155e47f86ec21659e145f8cc9e876e4480
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-09 07:01:40 -0700

    1-0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Description: prints random whole number and its sign
+ *
+ *Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+		printf("%i is negative", n);
+	else if (n > 0)
+		printf("%i is positive", n);
+	else
+		print("%i is zero", n);
+
+	printf("\n");
+	return (0);
+}
