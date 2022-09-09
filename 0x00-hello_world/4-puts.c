f944879f3fda445c83cb691846b45a7cc2344be1
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-09 02:30:44 -0700

    2-4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char *quote ="\"Programming is like building a multilingual puzzle,";
-	puts(quote);
+	char *quote = "\"Programming is like building a multilingual puzzle";
 
+	puts(quote);
 	return (0);
 }
