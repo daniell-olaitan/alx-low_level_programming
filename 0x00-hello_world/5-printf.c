ec12a5081a6b64197c918c2ffb4c892c9e38512b
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-09 03:01:24 -0700

    1-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the given quote to the stdout
+ *
+ * Return: always 0 if successful
+ */
+int main(void)
+{
+	char *quote = "with proper grammar, but the output is a piece of art,\n";
+
+	printf("%s", quote);
+	return (0);
+}
