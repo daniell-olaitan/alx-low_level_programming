938b12ef21bdd7f904d38b255e89dbf7f034f47e
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-09 02:24:28 -0700

    1-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the given string on the stdout
+ *
+ * Return: always 0, to the operating system if successful
+ */
+int main(void)
+{
+	char *quote ="\"Programming is like building a multilingual puzzle,";
+	puts(quote);
+
+	return (0);
+}
