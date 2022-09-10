43142b64e2b8d8982d08adc2af4c9cca0820c5dd
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-10 04:52:07 -0700

    1-12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *Description: prints two two-digit possible comb of digits
+ *Return: 0, if successful
+ */
+int main(void)
+{
+	int digit_1 = '0';
+	int digit_2;
+	int digit_3;
+	int digit_4;
+
+	while (digit_1 <= '9')
+	{
+		digit_2 = '0';
+		while (digit_2 <= '9')
+		{
+			digit_3 = digit_2;
+			while (digit_3 <= '9')
+			{
+				digit_4 = digit_2 + 1;
+				while (digit_4 <= '9')
+				{
+					putchar(digit_1);
+					putchar(digit_2);
+					putchar((int)' ');
+					if (digit_1 != '9' || digit_2 != '8' || digit_3 != '9' || digit_4 != '9')
+					{
+						putchar((int)',');
+						putchar((int)' ');
+					}
+
+					digit_4++;
+				}
+
+				digit_3++;
+			}
+
+			digit_2++;
+		}
+
+		digit_1++;
+	}
+
+	return (0);
+}
