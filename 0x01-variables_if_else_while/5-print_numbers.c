93ab1facbf653b5d2516656fe036ac64e3ff11b5
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-09 23:43:35 -0700

    1-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#inclucde <stdio.h>
+
+/**
+ * main - entry point
+ *Description: prints all single digit of base 10
+ *Return: 0, if successful
+ */
+int main(void)
+{
+	int number;
+
+	number = 0;
+	while(number < 10)
+		printf("%i", number);
+
+	printf("\n");
+
+	return (0);
+}
