c335b180c8b344299749b4b344dbee307bb285b1
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-13 08:54:09 -0700

    1-5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a given number
+ * @n: the number which sign is to be printed
+ *
+ * Return: 1 if n is +ve, 0 if 0, and -1 if -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+}
