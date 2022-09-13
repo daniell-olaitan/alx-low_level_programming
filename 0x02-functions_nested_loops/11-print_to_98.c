e72397cef876fdfb44345b4cb26234df6073cd54
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-13 15:32:34 -0700

    2-11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,24 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 99; ++i)
+	if (n <= 0)
 	{
-		printf("%d", i);
-		if (i < 98)
-			printf(", ");
+		for (i = n; i < 99; ++i)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
 	}
+	else
+	{
+		for (i = n; i >= 98; ++i)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
+
+	printf("\n");
 }
