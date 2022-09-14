f73beb0c69c52cf94c33947017e7160d6e0cf348
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-14 15:45:45 -0700

    17-16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int i;
+	int carry;
 	unsigned long tmp, tmp_b;
 	unsigned long n1, n2, b1, b2;
 
@@ -31,13 +32,13 @@ int main(void)
 		tmp = n1 + n2 + carry;
 		n1 = n2;
 		n2 = tmp;
-		tmp1 = b1 + b2 - carry * 1000;
+		tmp_b = b1 + b2 - carry * 1000;
 		b1 = b2;
-		b2 = tmp1;
+		b2 = tmp_b;
 		if (tmp >= 100)
-			printf("%lu%lu", tmp, tmp1);
+			printf("%lu%lu", tmp, tmp_b);
 		else
-			printf("%lu0%lu", tmp, tmp1);
+			printf("%lu0%lu", tmp, tmp_b);
 
 		if (i != 98)
 			printf(", ");
