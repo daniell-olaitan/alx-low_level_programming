6c965bad23eab54d3fe04590aa0888e4c2d73285
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-13 18:01:05 -0700

    1-15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 fibonacci series
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long tmp = 2;
+	unsigned long n1 = 1;
+	unsigned long n2 = 2;
+	unsigned long sum = 2;
+
+	for (tmp <= 4000000)
+	{
+		tmp = n1 + n2;
+		n1 = n2;
+		n2 = tmp;
+		if (tmp % 2 == 0)
+			sum += tmp;
+	}
+
+	printf("\n");
+	return (0);
+}
