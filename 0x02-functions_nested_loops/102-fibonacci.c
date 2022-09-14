0cb3f362adecdd2eb58ffdb4256f50107ee41477
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-13 17:31:03 -0700

    4-13

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	int i;
-	int tmp;
-	int n1 = 1;
-	int n2 = 2;
+	unsigned long tmp;
+	unsigned long n1 = 1;
+	unsigned long n2 = 2;
 
 	printf("1, 2, ");
 	for (i = 0; i <= 48; ++i)
 	{
 		tmp = n1 + n2;
-		n1 = n2;
+		1 = n2;
 		n2 = tmp;
 		printf("%d", tmp);
 		if (i < 48)
