d405891315798d6dfe459dab5a0d636d19f2e645
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-15 16:00:05 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 through 100 with a substitute for the multiples of 3 and 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i < 100)
+			printf(" ");
+	}
+
+	return (0);
+}
