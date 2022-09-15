ff3b03e7a84facb9a69dfe0b606d01622cd84e30
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-15 16:13:55 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle on the stdout
+ * @size: size of the triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; ++i)
+		{
+			for (j = 0; j < size - i; ++j)
+				_putchar(' ');
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
