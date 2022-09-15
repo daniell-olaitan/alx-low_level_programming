81da67137a9c2132bc4a3e055e663567955b01dc
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-15 14:56:58 -0700

    Numbers constitute the only universay language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the number from 0 through 14
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j <= 14; ++j)
+		{
+			if (j > 9)
+				_putchar((i / 10) + '0');
+
+			_putchar((i % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
