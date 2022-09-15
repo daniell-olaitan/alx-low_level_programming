278053c684edba51b7c6880126aa0e5119b86980
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-15 07:31:02 -0700

    1-2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two ints
+ * @a: first int for the multiplication
+ * @b: second int for the multiplication
+ *
+ * Return: the result of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int mul(int, int);
 void print_to_98(int);
 void print_times_table(int);
 
