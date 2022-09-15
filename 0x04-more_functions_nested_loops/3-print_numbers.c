bfdbf377af114da7a425b6eb4d473a8d8862be5c
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-15 14:22:46 -0700

    1-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 through 9
+ *
+ */
+void print_numbers(void)
+{
+	char number = '0';
+
+	while(number <= '0')
+		_putchar(number);
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,15 @@
 #ifndef MAIN_HEADER
 #define MAIN_HEADER
 
-int _putchar(char);
-void _puts(char*);
-void print_alphabet(void);
-void print_alphabet_x10(void);
 int _isupper(int);
 int _isdigit(int);
-int print_sign(int);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
 int mul(int, int);
-void print_to_98(int);
-void print_times_table(int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int);
+void print_diagonal(int);
+void print_square(int);
+void print_triangle(int);
 
 #endif
