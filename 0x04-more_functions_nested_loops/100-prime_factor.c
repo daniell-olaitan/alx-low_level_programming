3a46375b53b29078482e13c99e2580a45c85fa96
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-15 17:50:39 -0700

    Largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * isprime - checks if a number is a prime number
+ * @n: the number to be checked
+ *
+ * Return: 1 if number is a prime number, 0 otherwise
+ */
+int isprime(unsigned long n)
+{
+	int i;
+
+	if (n <= 1)
+		return (0);
+
+	i = 2;
+	while (i * i <= n)
+	{
+		if (n % i == 0)
+			return (0);
+
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - prints the largest prime factor of a number
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long n = 612852475143;
+	long div = 2;
+
+	while(1)
+	{
+		if (n % div == 0)
+		{
+			n = n / div;
+			if (n == 1)
+				break;
+		}
+		else
+		{
+			while (1)
+			{
+				div++;
+				if (isprime(div))
+					break;
+			}
+		}
+	}
+
+	printf("%ld\n", div);
+}
