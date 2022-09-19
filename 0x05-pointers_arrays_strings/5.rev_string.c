08a5b2c6b4e0a1e6ef120ac0908beddcc323558f
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-19 03:23:25 -0700

    A good engineer thinks in reverse and asks himself about the stylistic conssequences of the components and systems he proposes.

---
+++ 0x05-pointers_arrays_strings/5.rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a given string in reverse
+ * @s: the string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; i >= 0; --i)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+
+/**
+ * _strlen - computes and returns the length of a given string
+ *@s: the given string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; ++i)
+	{
+	}
+
+	return (i);
+}
