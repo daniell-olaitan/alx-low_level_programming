bfe2578785067900c59746ccba4994d6fed690ee
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-19 01:27:27 -0700

    This report, by its very length, depends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - computes and returns the length of a string
+ * @s: the string which length is to be returned
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s)
+		len++;
+
+	return (len);
+}
