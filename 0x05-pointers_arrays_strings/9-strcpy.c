2bc79a705ed975f45e14d627d60ecc6e0a5cc988
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-19 04:29:32 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string from source to destination
+ * @dest: the destination
+ * @src: the source
+ *
+ * Return: the destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
