837710378db4b69bcc1535facb0c0f58702e22f4
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-19 01:43:30 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a given string to the stdout
+ * @str: the string to be printed
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; s[i]; ++i)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
