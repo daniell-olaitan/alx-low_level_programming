d1b40fdf8abadf8c2528da268f22105619b38d02
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-22 00:37:09 -0700

    Numbers have life; they\'re not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - print an int on stdout
+ * @n: the given int
+ *
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n < 0)
+		_putchar('-');
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
+}
