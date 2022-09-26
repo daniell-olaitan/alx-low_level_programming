691de47f26525ac91bedac90ba36401b62b7aa5a
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-26 11:03:50 -0700

    Memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination
+ * @src: pointer to the source
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	dest = src;
+}
