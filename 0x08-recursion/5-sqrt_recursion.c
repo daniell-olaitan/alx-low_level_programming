b2d9c65e62a1b176eb717321f995f36e73b7f651
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-28 14:27:30 -0700

    Your subconscious is lookkng for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * sqrt_sub - finds the sqrt of an int
+ * @n: the number
+ * @a: the sqrt
+ *
+ * Return: sqrt
+ */
+int sqrt_sub(int n, int a)
+{
+	if (a * a > n)
+		return (-1);
+
+	if (a * a == b)
+		return (a);
+
+	return (sqrt_sub(n, a + 1));
+}
+
+/**
+ * _sqrt_recursion - computes the natural sqrt of a number
+ * @n: the given number
+ *
+ * Return: the computed sqrt
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_sub(n, 1));
+}
