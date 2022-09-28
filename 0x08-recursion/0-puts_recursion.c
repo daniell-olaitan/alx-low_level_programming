bbe31aebe00f3c76066de47b4b5c87c397d97297
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-28 08:58:26 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: the given string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (!(*s))
+		_putchar('\n');
+
+	_putchar(*s);
+	_puts_recursion(++s);
+}
