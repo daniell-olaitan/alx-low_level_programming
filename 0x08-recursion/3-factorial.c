4c239371a9a048e233e8b6291fd5fa59df3c2bf7
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-28 09:45:04 -0700

    You mustn\'t be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - computes the factorial of a number
+ * @n: the given number
+ *
+ * Return: the factorial of the number
+ */
+int factorial(int n)
+{
+	if (n == 0 || n == 1)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
