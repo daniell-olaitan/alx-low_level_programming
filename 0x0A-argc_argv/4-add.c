4d862aa445bc8a703f00243f0882a6a926719809
daniel-olaitan <daniell.olaitan@gmail.com>
2022-09-30 14:38:24 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command line args
+ * @argv: array of command line args
+ *
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, status, zero = 0, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; ++i)
+		{
+			status = atoi(argc[i]);
+			if (!status)
+				zero++;
+
+		}
+
+		if (argc - 1 == zero)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		else if (!zero)
+		{
+			for (i = 1; i < argc; ++i)
+				sum += atoi(argv[i]);
+
+			printf("%d\n", sum);
+			return (0);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	else
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+}
