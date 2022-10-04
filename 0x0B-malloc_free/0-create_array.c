8054ce5dee08633ef2d7013f0a6b17668b4b7043
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-04 04:19:54 -0700

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *str = malloc(sizeof(char) * size);
 
+	if (size == 0)
+		return (NULL);
 	if (str)
 	{
 		for (i = 0; i < size; ++i)
