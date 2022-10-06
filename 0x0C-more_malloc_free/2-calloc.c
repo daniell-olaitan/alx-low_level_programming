33f7ca515cda15c0d9d8513f4096b43677d4ec3a
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-06 04:09:04 -0700

    _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "funcdef.h"
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of elements of the array
+ * @size: size of each member
+ *
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	mem = malloc(nmemb * size);
+	if (!mem)
+	{
+		free(mem);
+		return (NULL);
+	}
+
+	else
+	{
+		return (_memset(mem, 0, nmemb * size);
+	}
+}
--- 0x0C-more_malloc_free/funcdef.h
+++ 0x0C-more_malloc_free/funcdef.h
@@ -4,6 +4,26 @@
 #include "main.h"
 
 /**
+ * _memset - sets a memory space to a given character
+ * @mem: the memory space
+ * @c: the character to set
+ * @size: the size of the memory to set
+ *
+ * Return: pointer to mem
+ */
+void *_memset(void *mem, char c, unsigned int size)
+{
+	unsigned int i;
+
+	for (i = 0; i < size; ++i)
+	{
+		*(mem + i) = c;
+	}
+
+	return (mem);
+}
+
+/**
  * _strlen - computes len of str
  * @str: the given str
  *
