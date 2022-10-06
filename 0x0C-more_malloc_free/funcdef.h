53f67193553fb9c56d0b9f2e525aae0ebc5955e2
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-06 03:20:08 -0700

    string_nconcat

---
+++ 0x0C-more_malloc_free/funcdef.h
@@ -0,0 +1,27 @@
+#ifndef DEF_H
+#define DEF_H
+
+#include "main.h"
+
+/**
+ * _strlen - computes len of str
+ * @str: the given str
+ *
+ * Return: len of str
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i = 0;
+
+	if (str)
+	{
+		for (i = 0; *str; ++i)
+		{
+			str++;
+		}
+	}
+
+	return (i);
+}
+
+#endif
