24d49f096cdbc420a558f3ea6b67e511b59fd554
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-10 15:19:12 -0700

    How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn\'t make it a leg

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name ? d->name : err));
-		printf("Age: ", (d->age ? d->age : 0));
-		printf("Owner: ", (d->owner ? d->owner : err));
+		printf("Age: %s\n", (d->age ? d->age : 0));
+		printf("Owner: %s\n", (d->owner ? d->owner : err));
 	}
 }
