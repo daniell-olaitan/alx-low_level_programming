1f14e20e9f1205b0d875549fd1841269718c42dc
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-12 04:16:25 -0700

    Correction

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+int _putchar(char);
 void print_name(char *, void (f*)(char *));
 void array_iterator(int *, size_t, void (*action)(int));
 int int_index(int *, int, int (*cmp)(int));
