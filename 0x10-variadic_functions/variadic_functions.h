273c6f6037bfe203f3e9ae2c69595459262214fd
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-13 04:21:31 -0700

    Correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stddef.h>
 
+int _putchar(char);
 int sum_them_all(const unsigned int, ...);
 void print_numbers(const char *, const unsigned int, ...);
 void print_strings(const char *, const unsigned int, ...);
