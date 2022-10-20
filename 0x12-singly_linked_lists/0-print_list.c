1493878e190db22893c8f0ec5ff2b3fb87386c94
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-20 11:26:40 -0700

    Debugged print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,14 @@ size_t print_list(const list_t *h)
 
 	if (h)
 	{
-	       	for (i = 0; h; ++i)
-       		{
+		for (i = 0; h; ++i)
+		{
 			if (h->str)
-		       		printf("[%u] %s\n", h->len, h->str);
-	       		else
-       				printf("[0] (nil)\n");
-       			h = h->next;
-       		}
+				printf("[%u] %s\n", h->len, h->str);
+			else
+				printf("[0] (nil)\n");
+			h = h->next;
+		}
 	}
 
 	return (i);
