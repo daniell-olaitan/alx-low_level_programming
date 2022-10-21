c48b512ddeefca04792f80fd8309c478d66aef50
daniel-olaitan <daniell.olaitan@gmail.com>
2022-10-20 17:32:33 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees the memory space taken up by a list
+ * @head: pointer to list head
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while(head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}
