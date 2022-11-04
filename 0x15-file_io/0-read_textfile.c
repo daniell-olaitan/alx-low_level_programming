02582dd8f9cc90491f32ab6af278682c28da4677
daniel-olaitan <daniell.olaitan@gmail.com>
2022-11-04 00:10:37 -0700

    Tread softly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,6 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	rd_cnt = read(fd, buffer, letters);
 	wr_cnt = write(STDOUT_FILENO, buffer, rd_cnt);
