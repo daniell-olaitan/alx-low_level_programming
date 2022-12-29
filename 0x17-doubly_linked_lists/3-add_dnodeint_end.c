af990c4432be31b33c12f47920bb5eda4feaf582
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-29 13:19:54 +0100

    task 7 corrections

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,4 +1,35 @@
-#include "create_node.h"
+#include "lists.h"
+
+/**
+ * create_node - creates a dlistint_t node
+ *
+ * Return: pointer to the newly created node, or NULL if unsuccessful
+ */
+dlistint_t *create_node(void)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->next = malloc(sizeof(dlistint_t));
+	if (node->next == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->prev = malloc(sizeof(dlistint_t));
+	if (node->prev == NULL)
+	{
+		free(node->next);
+		free(node);
+		return (NULL);
+	}
+
+	return (node);
+}
 
 /**
  * add_dnodeint_end - adds a new node at the end of a dlistint_t list
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,35 @@
-#include "create_node.h"
+#include "lists.h"
+
+/**
+ * create_node - creates a dlistint_t node
+ *
+ * Return: pointer to the newly created node, or NULL if unsuccessful
+ */
+dlistint_t *create_node(void)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->next = malloc(sizeof(dlistint_t));
+	if (node->next == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->prev = malloc(sizeof(dlistint_t));
+	if (node->prev == NULL)
+	{
+		free(node->next);
+		free(node);
+		return (NULL);
+	}
+
+	return (node);
+}
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
