c72c439e0ec21092c29eff8b37d6c91d4689250a
daniell-olaitan <daniell.olaitan@gmail.com>
2022-12-31 19:00:36 +0100

    task 7 corrections

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,6 +31,39 @@ dlistint_t *_create_node(void)
 	return (node);
 }
 
+
+/**
+ * insert_node - inserts a given node at the given index
+ * @h: pointer to the head of the list
+ * @c: pointer to the current node in the list
+ * @p: pointer to the previous node in the list
+ * @n: pointer the node to be inserted
+ * @i: index
+ *
+ * Return: pointer to the inserted node
+ */
+dlistint_t *insert_node(dlistint_t **h, dlistint_t *c, dlistint_t *p, dlistint_t *n, unsigned int i)
+{
+	n->next = c;
+	if (c == NULL)
+	{
+		n->prev = p;
+		p->next = n;
+	}
+	else
+	{
+		n->prev = c->prev;
+		if (i == 0)
+			*h = n;
+		else
+			c->prev->next = n;
+
+		c->prev = n;
+	}
+
+	return (n);
+}
+
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: pointer to the head of the list
@@ -61,24 +94,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		for (i = 0; current != NULL || idx == i; ++i)
 		{
 			if (idx == i)
-			{
-				node->next = current;
-				if (current == NULL)
-				{
-					node->prev = prev;
-					prev->next = node;
-				}
-				else
-				{
-					node->prev = current->prev;
-					if (idx == 0)
-						*h = node;
-					else
-						current->prev->next = node;
-					current->prev = node;
-				}
-				return (node);
-			}
+				return (insert_node(h, current, prev, node, i));
+
 			prev = current;
 			if (current != NULL)
 				current = current->next;
