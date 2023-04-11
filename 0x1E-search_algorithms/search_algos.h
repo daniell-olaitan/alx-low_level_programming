b71eed294f9c59b806081cb1749cfca900c1cb38
daniell-olaitan <daniell.olaitan@gmail.com>
2023-04-11 23:51:39 +0100

    Linear Search Algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* Standard Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif
